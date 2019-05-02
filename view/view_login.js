bb35b62179a1afb84ce9c9efaf21af2339809d49
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2019-05-01 22:47:10 -0300

    add login screen

---
+++ view/view_login.js
@@ -0,0 +1,7 @@
+const BT_LOGIN = $("#bt-login");
+const BT_EMAIL = $("#inp-email");
+const BT_PASSWORD = $("#inp-password");
+
+BT_LOGIN.click(function(){
+	window.location.href = "project.html";
+});
